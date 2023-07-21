5ac74719d6deac287b9f89adca19dc1b0382e0e6
Peter Chen <peterch0829@gmail.com>
2023-07-21 11:07:25 -0400

    Initial Commit

--- src/lottery.ts
+++ src/lottery.ts
@@ -1,162 +1,16 @@
 import {
-  AdminChanged as AdminChangedEvent,
-  BeaconUpgraded as BeaconUpgradedEvent,
-  BorrowedTicket as BorrowedTicketEvent,
-  ClaimedReward as ClaimedRewardEvent,
-  Initialized as InitializedEvent,
-  JoinedLottery as JoinedLotteryEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
-  StartedLottery as StartedLotteryEvent,
-  Upgraded as UpgradedEvent,
   WinnerSelected as WinnerSelectedEvent
 } from "../generated/Lottery/Lottery"
 import {
-  AdminChanged,
-  BeaconUpgraded,
-  BorrowedTicket,
-  ClaimedReward,
-  Initialized,
-  JoinedLottery,
-  OwnershipTransferred,
-  StartedLottery,
-  Upgraded,
   WinnerSelected
 } from "../generated/schema"
-
-export function handleAdminChanged(event: AdminChangedEvent): void {
-  let entity = new AdminChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousAdmin = event.params.previousAdmin
-  entity.newAdmin = event.params.newAdmin
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleBeaconUpgraded(event: BeaconUpgradedEvent): void {
-  let entity = new BeaconUpgraded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.beacon = event.params.beacon
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleBorrowedTicket(event: BorrowedTicketEvent): void {
-  let entity = new BorrowedTicket(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.borrower = event.params.borrower
-  entity.tokenId = event.params.tokenId
-  entity.wrappedTokenId = event.params.wrappedTokenId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleClaimedReward(event: ClaimedRewardEvent): void {
-  let entity = new ClaimedReward(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.amount = event.params.amount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleInitialized(event: InitializedEvent): void {
-  let entity = new Initialized(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.version = event.params.version
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleJoinedLottery(event: JoinedLotteryEvent): void {
-  let entity = new JoinedLottery(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.tokenId = event.params.tokenId
-  entity.buyer = event.params.buyer
-  entity.amount = event.params.amount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleStartedLottery(event: StartedLotteryEvent): void {
-  let entity = new StartedLottery(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.timestamp = event.params.timestamp
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleUpgraded(event: UpgradedEvent): void {
-  let entity = new Upgraded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.implementation = event.params.implementation
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+import { Value } from "@graphprotocol/graph-ts";
 
 export function handleWinnerSelected(event: WinnerSelectedEvent): void {
   let entity = new WinnerSelected(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
+    event.transaction.from.toHex()
   )
-  entity.winners = event.params.winners
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.winners = Value.fromAddressArray(event.params.winners).toBytesArray();
 
   entity.save()
 }
